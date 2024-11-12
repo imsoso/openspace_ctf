b0276cc1e76bb8d5c322fb96fe6b6e916c7e46fd
Soso <husosong@163.com>
2024-11-12 17:35:58 +0800

    Improvement: Fomating

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
 
-
-
-
 contract VaultExploiter is Test {
     Vault public vault;
     VaultLogic public logic;
 
-    address owner = address (1);
-    address palyer = address (2);
+    address owner = address(1);
+    address palyer = address(2);
 
     function setUp() public {
         vm.deal(owner, 1 ether);
@@ -23,7 +20,6 @@ contract VaultExploiter is Test {
 
         vault.deposite{value: 0.1 ether}();
         vm.stopPrank();
-
     }
 
     function testExploit() public {
